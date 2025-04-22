05e1a6f37057aa967de331a98bf1ce9a662695cb
unknown <leandrozacariaz18@gmail.com>
2025-04-22 03:42:50 -0300

    modificando cloudinary

--- product-catalog-service/src/main/java/com/ecommerce/product_catalog_service/config/CloudinaryConfig.java
+++ product-catalog-service/src/main/java/com/ecommerce/product_catalog_service/config/CloudinaryConfig.java
@@ -3,24 +3,29 @@ package com.ecommerce.product_catalog_service.config;
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-/* 
 @Configuration
 public class CloudinaryConfig {
 
     @Bean
+    @ConditionalOnProperty(name = "cloudinary.enabled", havingValue = "true")
     public Cloudinary cloudinary(
-        @Value("${cloudinary.cloud-name}") String cloudName,
-        @Value("${cloudinary.api-key}") String apiKey,
-        @Value("${cloudinary.api-secret}") String apiSecret
+        @Value("${cloudinary.cloud-name:}") String cloudName,
+        @Value("${cloudinary.api-key:}") String apiKey,
+        @Value("${cloudinary.api-secret:}") String apiSecret
     ) {
-        System.out.println("Cloudinary Config - Cloud Name: " + cloudName + ", API Key: " + apiKey);
+        if (cloudName.isBlank() || apiKey.isBlank() || apiSecret.isBlank()) {
+            throw new IllegalStateException("Cloudinary config is missing");
+        }
+
         return new Cloudinary(ObjectUtils.asMap(
             "cloud_name", cloudName,
             "api_key", apiKey,
             "api_secret", apiSecret
         ));
     }
-}*/
+
+}
